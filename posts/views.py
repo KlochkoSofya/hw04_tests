30bf0289058d8567defe37a7ab91f1b25b98994a
Sofya <sofya.s.frolova@gmail.com>
2021-02-19 16:58:09 +0300

    After PEP8 checking

--- posts/views.py
+++ posts/views.py
@@ -47,8 +47,8 @@ def profile(request, username):
 
 
 def post_view(request, username, post_id):
-    author=get_object_or_404(User, username=username)
-    needed_post=get_object_or_404(Post, id=post_id, author__username=username)
+    author = get_object_or_404(User, username=username)
+    needed_post = get_object_or_404(Post, id=post_id, author__username=username)
     context = {'author': author, 'post': needed_post}
     return render(request, 'posts/post.html', context)
 
