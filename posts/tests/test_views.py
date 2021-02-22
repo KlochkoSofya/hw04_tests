c556ebab7b91b6e886ac769b913b178a50feb85f
Sofya <sofya.s.frolova@gmail.com>
2021-02-22 20:41:20 +0300

    After 2nd review

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -134,25 +134,3 @@ class PostViewsTests(TestCase):
         self.assertEqual(post_text, 'Текст')
         self.assertEqual(post_author.username, 'username')
         self.assertEqual(post_group.title, 'Группа-1')
-
-    def test_about_page_accessible_by_name(self):
-        """URL, генерируемый при помощи имени static_pages:about, доступен."""
-        about_list = {
-            reverse('about:author'): 200,
-            reverse('about:tech'): 200
-        }
-        for reverse_name, expected in about_list.items():
-            with self.subTest(reverse_name=reverse_name):
-                response = self.guest_client.get(reverse_name)
-                self.assertEqual(response.status_code, expected)
-
-    def test_about_page_uses_correct_template(self):
-
-        templates_pages_names = {
-            'about/author.html': reverse('about:author'),
-            'about/tech.html': reverse('about:tech'),
-        }
-        for template, reverse_name in templates_pages_names.items():
-            with self.subTest(reverse_name=reverse_name):
-                response = self.guest_client.get(reverse_name)
-                self.assertTemplateUsed(response, template)
